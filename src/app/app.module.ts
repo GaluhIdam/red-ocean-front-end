7cb11b53601cb1cfb2de6771545a8133489581ee
Galuh Danutirto <galuh.danutirto@opencloud.id>
2023-12-14 14:14:37 +0700

    [initial] intializer angular to github repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './modules/home/home.module';
+import { UserLayoutComponent } from './shared/user-layout/user-layout.component';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLayoutComponent,
+    AdminLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
