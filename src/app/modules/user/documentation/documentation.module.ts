204b8302ed6d0e99a22988eec21990be0237225b
Galuh Danutirto <galuh.danutirto@opencloud.id>
2024-01-12 16:30:59 +0700

    [add] initial document modules

--- src/app/modules/user/documentation/documentation.module.ts
+++ src/app/modules/user/documentation/documentation.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DocumentationRoutingModule } from './documentation-routing.module';
 import { DocumentationComponent } from './documentation.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { HighlightComponent } from './components/highlight/highlight.component';
 
 
 @NgModule({
   declarations: [
-    DocumentationComponent
+    DocumentationComponent,
+    HeroComponent,
+    HighlightComponent
   ],
   imports: [
     CommonModule,
