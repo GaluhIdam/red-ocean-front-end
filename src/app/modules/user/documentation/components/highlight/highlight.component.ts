3d9693f4d420d3833a2a3ede0dbc5bb136576f92
Galuh Danutirto <galuh.danutirto@opencloud.id>
2024-01-19 04:41:10 +0700

    [update] update documentation page

--- src/app/modules/user/documentation/components/highlight/highlight.component.ts
+++ src/app/modules/user/documentation/components/highlight/highlight.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { CardArticleDTO } from 'src/app/shared/components/card-article/card-article.interface';
 
 @Component({
   selector: 'app-highlight',
   templateUrl: './highlight.component.html',
-  styleUrls: ['./highlight.component.css']
+  styleUrls: ['./highlight.component.css'],
 })
 export class HighlightComponent {
+  faCheckCircle = faCheckCircle;
 
+  article: CardArticleDTO[] = [
+    {
+      image: './assets/article-image.png',
+      date: 'Saturday, November 11 2023',
+      title: 'Lorem Ipsum is simply dummy text of the printing',
+      description:
+        'ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis',
+    },
+    {
+      image: './assets/article-image.png',
+      date: 'Saturday, November 11 2023',
+      title: 'Lorem Ipsum is simply dummy text of the printing',
+      description:
+        'ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis',
+    },
+    {
+      image: './assets/article-image.png',
+      date: 'Saturday, November 11 2023',
+      title: 'Lorem Ipsum is simply dummy text of the printing',
+      description:
+        'ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis',
+    },
+    {
+      image: './assets/article-image.png',
+      date: 'Saturday, November 11 2023',
+      title: 'Lorem Ipsum is simply dummy text of the printing',
+      description:
+        'ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis',
+    },
+  ];
 }
--- src/app/modules/user/documentation/documentation.module.ts
+++ src/app/modules/user/documentation/documentation.module.ts
@@ -5,17 +5,16 @@ import { DocumentationRoutingModule } from './documentation-routing.module';
 import { DocumentationComponent } from './documentation.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { HighlightComponent } from './components/highlight/highlight.component';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SharedModule } from 'src/app/shared/components/shared.module';
 
 @NgModule({
-  declarations: [
-    DocumentationComponent,
-    HeroComponent,
-    HighlightComponent
-  ],
+  declarations: [DocumentationComponent, HeroComponent, HighlightComponent],
   imports: [
     CommonModule,
-    DocumentationRoutingModule
-  ]
+    FontAwesomeModule,
+    DocumentationRoutingModule,
+    SharedModule,
+  ],
 })
-export class DocumentationModule { }
+export class DocumentationModule {}
